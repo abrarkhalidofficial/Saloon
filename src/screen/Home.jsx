c78ec2657053f0c10f77deb08358e42424cb67c2
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-21 18:06:50 +0500

    done

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import discoverleft from "../assets/discover-left.png";
 import discovercontent from "../assets/discover-content.png";
 import discoverright from "../assets/discover-right.png";
@@ -10,11 +10,36 @@ import productsleft from "../assets/products-left.png";
 import productsright from "../assets/products-right.png";
 
 function Home() {
+  const [selectedItem, setSelectedItem] = useState([]);
   const list = [
     {
-      name: "S",
+      name: "Sauna",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      image: productsright,
+      defaultChecked: true,
+    },
+    {
+      name: "Whirlpool",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      image: productsright,
+    },
+    {
+      name: "Jet bath",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      image: productsright,
     },
   ];
+  useEffect(() => {
+    list
+      .filter((_item, i) => i === 0)
+      .map((item) => {
+        setSelectedItem(item);
+      });
+  }, []);
+
   return (
     <>
       <section className="discover__Section">
@@ -58,8 +83,23 @@ function Home() {
               <img src={productsleft} alt="products___left" />
             </div>
             <div className="products__all__button">
-              <button className="product__button ">Sauna</button>
-              <button className="product__button">Whirlpool</button>
+              {list.map((item) => (
+                <div className="product__button">
+                  <input
+                    type="radio"
+                    className="product__button__input"
+                    name="product__button"
+                    defaultChecked={item.defaultChecked}
+                    onClick={() => {
+                      setSelectedItem(item);
+                      console.log("hey");
+                    }}
+                  />
+                  <div className="product__button__content">{item.name}</div>
+                </div>
+              ))}
+
+              {/* <button className="product__button">Whirlpool</button>
               <button className="product__button">Jet bath</button>
               <button className="product__button">Steam room</button>
               <button className="product__button">Massage</button>
@@ -70,21 +110,22 @@ function Home() {
                 Clay or herbal body masks
               </button>
               <button className="product__button">Reflexology </button>
-              <button className="product__button">Waxing.</button>
+              <button className="product__button">Waxing.</button> */}
             </div>
           </div>
           <div className="products___right">
             <div className="products___right__img">
-              <img src={productsright} alt="products___right" />
+              <img src={selectedItem?.image} alt="products___right" />
             </div>
             <div className="products___right___heading__info">
-              <div className="products___right___heading">Sauna</div>
-              <div className="products___right___heading__short">Sauna</div>
+              <div className="products___right___heading">
+                {selectedItem?.name}
+              </div>
+              <div className="products___right___heading__short">
+                {selectedItem?.name}
+              </div>
               <div className="products___right___info">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                {selectedItem?.description}
               </div>
               <div className="products___right___button">Book Now</div>
             </div>
